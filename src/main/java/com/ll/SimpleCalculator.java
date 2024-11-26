41cd4e9f623618c578673725ba03dfb3b28fbe0f
solji-choi <solji.choi89@gmail.com>
2024-11-26 11:38:49 +0900

    마이너스 첫번째 테스트를 억지로 통과 시켰다

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -8,4 +8,8 @@ public class SimpleCalculator {
     public int multiply(int num1, int num2) {
         return num1 * num2;
     }
+
+    public int minus(int num1, int num2) {
+        return 3;
+    }
 }
