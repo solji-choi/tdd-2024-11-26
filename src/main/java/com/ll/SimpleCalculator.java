d39c8df7ee35a24955d8c2d39437a233137641f2
solji-choi <solji.choi89@gmail.com>
2024-11-26 11:16:11 +0900

    리팩토링을 통해 정상적으로 작동하게 만들었다

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -2,10 +2,6 @@ package com.ll;
 
 public class SimpleCalculator {
     public int plus(int num1, int num2) {
-        if(num1 == 2 && num2 == 10) {
-            return 12;
-        }
-
-        return 3;
+        return num1 + num2;
     }
 }
